50ea15b1bf6a183e43cf6378144f1b1b15adf735
swhoosh <70953480+swhoosh@users.noreply.github.com>
2026-03-16 09:20:10 +0700

    nvim: enable quiting nvim using capitalized characters

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -118,6 +118,11 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.cmd 'command! W w'
+vim.cmd 'command! Q q'
+vim.cmd 'command! WQ wq'
+vim.cmd 'command! Wq wq'
+
 -- format on save
 vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*.go',
