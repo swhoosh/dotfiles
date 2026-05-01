c9250c280512a711fbcf9d8d5b472171cae5d7c7
kiartnarin.u <70953480+swhoosh@users.noreply.github.com>
2026-05-01 13:44:07 +0700

    plugin: add tresitter-context

---
+++ .config/nvim/lua/custom/plugins/treesitter-context.lua
@@ -0,0 +1,11 @@
+-- Sticky "where am I" header: function/method/class context at top of window (Go + other TS langs).
+-- See :help treesitter-context
+return {
+  'nvim-treesitter/nvim-treesitter-context',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  event = { 'BufReadPost', 'BufNewFile' },
+  opts = {
+    max_lines = 4,
+    multiline_threshold = 4,
+  },
+}
